147e0e656a602d5ef331b73024863585d5b078a7
Kyrylo Bitsay <service@kyrylo.tech>
2025-10-22 19:43:18 +0300

    Update TaskTags.py

--- app/Objects/tasks/TaskTags.py
+++ app/Objects/tasks/TaskTags.py
@@ -1,3 +1,5 @@
+import uuid
+
 from sqlalchemy import Column, String, DateTime, ForeignKey
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.sql import func
@@ -17,6 +19,6 @@ class TaskTag(Base):
     updated_at = Column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now())
 
 class TaskTagBase(PydModel):
-    id: UUID
+    id: uuid.UUID
     name: str
     color: str
